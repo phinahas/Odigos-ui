12b4756aa524f1eb047b26fbed63725c96a73766
Phinahas Philip <phinahasphilip2000@gmail.com>
2023-10-21 22:04:42 +0530

    UI&UX: User profile screen made, added Logout function only.

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-import {API_URL} from './config/constants'
+import {API_URL,DEV_API_URL} from './config/constants'
 
 const instance = axios.create({
-    baseURL:API_URL,
+    baseURL:DEV_API_URL,
 });
 
 export default instance
--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
 module.exports = {
-    API_URL: 'https://odigos-server.onrender.com/api'
+    API_URL: 'https://odigos-server.onrender.com/api',
+    DEV_API_URL: 'http://localhost:8080/api'
 }
